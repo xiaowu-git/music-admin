f3064f0844e133b7163523192261151b22760e42
xiaowu-git <2503677254@qq.com>
2019-05-05 22:05:27 +0800

    init

---
+++ src/main/java/com/music/musicadmin/MusicAdminApplication.java
@@ -0,0 +1,13 @@
+package com.music.musicadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music/musicadmin/MusicAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.music.musicadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
